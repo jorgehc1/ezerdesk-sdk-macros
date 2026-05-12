9819f9f32d89a777d8840dc23f2fd772848990c4
Jorge Hernández <jorgeenanin@gmail.com>
2026-05-11 21:43:21 -0500

    actualizacion

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,17 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let expanded = quote! {
         #input_fn
 
-        #[no_mangle]
+        #[unsafe(no_mangle)]
         pub extern "C" fn alloc(size: usize) -> *mut u8 {
             ::ezerdesk_sdk::allocate(size)
         }
 
-        #[no_mangle]
+        #[unsafe(no_mangle)]
         pub extern "C" fn deallocate(ptr: *mut u8, size: usize) {
             ::ezerdesk_sdk::deallocate(ptr, size);
         }
 
-        #[no_mangle]
+        #[unsafe(no_mangle)]
         pub extern "C" fn on_event(ptr: *const u8, len: usize) -> i32 {
             // Protección contra desbordamientos y punteros nulos
             if ptr.is_null() || len == 0 {
